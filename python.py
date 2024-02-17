79f6ed02e2b9b004cc89761eb03e7f08c69cc8be
dema98HH <w.demenkow@gmail.com>
2024-02-17 19:16:48 +0100

    [create]: .py after pull from main

--- python.py
+++ python.py
@@ -4,14 +4,14 @@ print('hi from user1!')
 
 
 
-<<<<<<< HEAD
+
 
 print('hot from user2')
 
 print('hot from user2 v2')
 
-=======
+
 print('hot from user2')
 
 print('hot from user2 v2')
->>>>>>> 0e69ee2 ([add]: hi from user2 v2)
+
